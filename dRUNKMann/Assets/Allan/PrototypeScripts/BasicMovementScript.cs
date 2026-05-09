d74d29eb782d2e94837bb5a27754cc0f1c735371
ValleyPenguin <valleypenguin@gmail.com>
2026-05-09 09:45:58 -0600

    Setup Inputs + Started Movement Script

---
+++ dRUNKMann/Assets/Allan/PrototypeScripts/BasicMovementScript.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BasicMovementScript : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float diveHeight;
+    [SerializeField] private float diveDirectionalStrength;
+    [SerializeField] private float groundCheckSphereVerticalOffset;
+    [SerializeField] private float groundCheckSphereRadius = 2f;
+    [SerializeField] private GameObject playerCapsule;
+    [SerializeField] private Rigidbody rb;
+
+    private Vector2 moveInput;
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    public void Dive(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            Debug.Log("Diving now!");
+        }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(new Vector3(playerCapsule.transform.position.x, playerCapsule.transform.position.y - groundCheckSphereVerticalOffset, playerCapsule.transform.position.y), 2f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(new Vector3(playerCapsule.transform.position.x, playerCapsule.transform.position.y - groundCheckSphereVerticalOffset, playerCapsule.transform.position.z), groundCheckSphereRadius);
+    }
+}
