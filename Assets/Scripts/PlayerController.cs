5b14bde96ce9373eff8906100730bbe2717ac775
Nejat Alper Satiroglu <n.satiroglu265@mybvc.ca>
2026-05-09 12:59:22 -0600

    Player Rotation added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,22 @@ public class PlayerController : MonoBehaviour
     float playerMovementX;
     float playerMovementY;
     [SerializeField] float movementSpeed = 10f;
+    [SerializeField] float rotationSpeed = 10f;
     
     void Update()
     {
-        playerMovementX = Input.GetAxis("Horizontal");
-        playerMovementY = Input.GetAxis("Vertical");
+        playerMovementX = Input.GetAxisRaw("Horizontal");
+        playerMovementY = Input.GetAxisRaw("Vertical");
         Vector3 movement = new Vector3(playerMovementX, 0f, playerMovementY);
+        movement.Normalize();
         
-        transform.Translate(movement * (movementSpeed * Time.deltaTime));
+        transform.Translate(  movementSpeed * Time.deltaTime * movement,Space.World);
+
+        if (movement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);            
+        }
         
     }
     
